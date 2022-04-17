7078b749b09d76039cda23e4fc836b8cdca101df
Derek <harrison.derek.w@gmail.com>
2022-04-17 21:07:13 +0200

    replace computation of matrix inverse with gauss-jordan method

--- two-bulb-impn/lib.cpp
+++ two-bulb-impn/lib.cpp
@@ -16,6 +16,236 @@
 std::vector<std::vector<double>> bulb1_t;
 std::vector<std::vector<double>> bulb2_t;
 
+double ** mat2D(int n) {
+
+    double ** mat = new double * [n];
+
+    for(int i = 0; i < n; ++i)
+        mat[i] = new double[n];
+
+    return mat;
+}
+
+void free_mat2D(double ** mat, int n) {
+
+    for(int i = 0; i < n; ++i)
+        delete [] mat[i];
+
+    delete [] mat;
+}
+
+void get_order(double ** mat, int n, double * order_arr) {
+    for(int row = 0; row < n; ++row) {
+        int order = 0;
+        for(int c = 0; c < n; ++c) {
+            if(mat[row][c] == 0) {
+                order++;
+            }
+        }
+        order_arr[row] = order;
+    }
+}
+
+void merge(oa_elem_t A[], int p, int q, int r) {
+    int size_r, size_l;
+    int i, j;
+    size_l = q - p + 1;
+    size_r = r - q;
+    oa_elem_t L[size_l+1];
+    oa_elem_t R[size_r+1];
+    i = 0;
+    j = 0;
+    for(int n = p; n < q + 1; ++n) {
+        L[i] = A[n];
+        ++i;
+    }
+    L[size_l].val = MAX_INT;
+    for(int n = q + 1; n < r + 1; ++n) {
+        R[j] = A[n];
+        ++j;
+    }
+    R[size_r].val = MAX_INT;
+    i = 0;
+    j = 0;
+    for(int n = p; n < r + 1; ++n) {
+        if(L[i].val < R[j].val) {
+            A[n] = L[i];
+            ++i;
+        }
+        else {
+            A[n] = R[j];
+            ++j;
+        }
+    }
+}
+
+void merge_sort(oa_elem_t A[], int p, int r) {
+    int q;
+    if(p < r) {
+        q = (p + r)/2;
+        merge_sort(A, p, q);
+        merge_sort(A, q + 1, r);
+        merge(A, p, q, r);
+    }
+}
+
+void mergesort(oa_elem_t A[], int size) {
+    merge_sort(A, 0, size - 1);
+}
+
+
+
+void make_ordered_mat(double ** mat, int n, double * order_arr, double ** ordered_mat) {
+
+    oa_elem_t * order_array = new oa_elem_t[n];
+
+    for(int row = 0; row < n; ++row) {
+        order_array[row].old_row = row;
+        order_array[row].val = order_arr[row];
+    }
+
+    mergesort(order_array, n);
+
+    for(int row = 0; row < n; ++row) {
+        for(int c = 0; c < n; ++c) {
+            int old_row = order_array[row].old_row;
+            ordered_mat[row][c] = mat[old_row][c];
+        }
+    }
+}
+
+int count_leading_zeros(double ** mat, int n, int row) {
+
+    int count_lz = 0;
+
+    for(int c = 0; c < n; ++c) {
+        if(mat[row][c] == 0) {
+            count_lz++;
+        }
+        else {
+            break;
+        }
+    }
+
+    return count_lz;
+}
+
+void mat_mult_sq(double ** A, double ** A_inv, int n, double ** mat_res) {
+
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n; ++j) {
+            double sum_loc = 0;
+
+            for(int k = 0; k < n; ++k) {
+                sum_loc = sum_loc + A[i][k] * A_inv[k][j];
+            }
+
+            mat_res[i][j] = sum_loc;
+        }
+    }
+}
+
+
+void gauss_jordan(double ** mat, int n, double ** mat_inv) {
+
+    double ** mat_check = mat2D(n);
+    double ** mat_check_ordered = mat2D(n);
+    double ** mat_ref = mat2D(n);
+    double ** mat_ordered = mat2D(n);
+    double ** mat_inv_ordered = mat2D(n);
+    double * order_arr = new double[n];
+
+
+    for(int row = 0; row < n; ++row) {
+        for(int c = 0; c < n; ++c) {
+            if(c == row) {
+                mat_inv[row][c] = 1.0;
+            }
+            else {
+                mat_inv[row][c] = 0.0;
+            }
+        }
+    }
+
+    get_order(mat, n, order_arr);
+
+    make_ordered_mat(mat, n, order_arr, mat_ordered);
+
+    make_ordered_mat(mat, n, order_arr, mat_check_ordered);
+
+    make_ordered_mat(mat_inv, n, order_arr, mat_inv_ordered);
+
+    for(int row = 0; row < n; ++row) {
+        for(int c = 0; c < n; ++c) {
+            mat_check[row][c] = mat_check_ordered[row][c];
+            mat_ref[row][c] = mat_ordered[row][c];
+            mat_inv[row][c] = mat_inv_ordered[row][c];
+        }
+    }
+
+    // Convert to row echelon form
+    for(int c = 0; c < n; ++c) {
+
+        // Sort if under threshold
+        if(fabs(mat_ref[c][c]) < 1e-8) {
+            get_order(mat_ref, n, order_arr);
+
+            make_ordered_mat(mat_ref, n, order_arr, mat_ordered);
+
+            make_ordered_mat(mat_inv, n, order_arr, mat_inv_ordered);
+
+            make_ordered_mat(mat_check, n, order_arr, mat_check_ordered);
+
+            for(int row = 0; row < n; ++row) {
+                for(int c = 0; c < n; ++c) {
+                    mat_ref[row][c] = mat_ordered[row][c];
+                    mat_inv[row][c] = mat_inv_ordered[row][c];
+                    mat_check[row][c] = mat_check_ordered[row][c];
+                }
+            }
+        }
+
+        // Normalize matrix row
+        for(int col = c + 1; col < n; ++col) {
+            mat_ref[c][col] = mat_ref[c][col] / (mat_ref[c][c] + 1e-13);
+        }
+
+        // Update row matrix inverse
+        for(int col = 0; col < n; ++col) {
+            mat_inv[c][col] = mat_inv[c][col] / (mat_ref[c][c] + 1e-13);
+        }
+
+        mat_ref[c][c] = 1.0;
+
+        // Delete elements in rows below
+        for(int row = c + 1; row < n; ++row) {
+            if(mat_ref[row][c] != 0) {
+                for(int col = c + 1; col < n; ++col) {
+                    mat_ref[row][col] = -1.0 * mat_ref[row][c] * mat_ref[c][col] + mat_ref[row][col];
+                }
+                for(int col = 0; col < n; ++col) {
+                    mat_inv[row][col] = -1.0 * mat_ref[row][c] * mat_inv[c][col] + mat_inv[row][col];
+                }
+                mat_ref[row][c] = 0;
+            }
+        }
+
+    }
+
+    // Backtracing
+    for(int c = n - 1; c > 0; --c) {
+        for(int row = c - 1; row > -1; row--) {
+            if(mat_ref[row][c] != 0) {
+                for(int col = 0; col < n; col++) {
+                    mat_inv[row][col] = -1.0 * mat_ref[row][c] * mat_inv[c][col] + mat_inv[row][col];
+                }
+                mat_ref[row][c] = 0;
+            }
+        }
+    }
+
+}
+
 void init_diffusivities(p_params_t & p_params, int n) {
     p_params.D = mat2D(n);
     
@@ -52,24 +282,6 @@ void set_frac_comp3(c_data_t & comp_data) {
     comp_data.bulb_data.mol_fracs_bulb2.x[n - 1] = 1.0 - sum_loc2;
 }
 
-double ** mat2D(int n) {
-    double ** r = new double * [n];
-    for(int i = 0; i < n; ++i) {
-        r[i] = new double[n];
-    }
-
-    return r;
-}
-
-void free_mat2D(double ** mat, int n) {
-
-    for(int i = 0; i < n; ++i)
-        delete [] mat[i];
-
-    delete [] mat;
-}
-
-
 double determinant(double ** mat, int n) {
     double det = 0;
 
@@ -82,7 +294,7 @@ double determinant(double ** mat, int n) {
     }
 
     if(n > 2) {
-    	double ** M = mat2D(n - 1);
+        double ** M = mat2D(n - 1);
 
         for(int c = 0; c < n; ++c) {
             for(int i = 1; i < n; ++i) {
@@ -168,6 +380,11 @@ void compute_mat_inv(double ** mat, int n, double ** mat_inv) {
     free_mat2D(adj_mat, n);
 }
 
+void compute_mat_inv2(double ** mat, int n, double ** mat_inv) {
+
+    gauss_jordan(mat, n, mat_inv);
+}
+
 void mat_prod(double ** mat1, double ** mat2, int n, double ** prod) {
     for(int i = 0; i < n; ++i) {
         for(int j = 0; j < n; ++j) {
@@ -218,7 +435,7 @@ void compute_coeff_matrix(int flux_node,
         }
     }
     
-    compute_mat_inv(comp_data.coeff_mat_boundary[flux_node].A,
+    compute_mat_inv2(comp_data.coeff_mat_boundary[flux_node].A,
                     n - 1,
                     comp_data.coeff_mat_boundary[flux_node].A_inv);
 }
