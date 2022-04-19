71faa94ae78bd1c9381e45a1f6ac2587a2ab9bce
Derek <harrison.derek.w@gmail.com>
2022-04-20 00:57:37 +0200

    update comments and refactor

--- two-bulb-impn/lib_gauss.cpp
+++ two-bulb-impn/lib_gauss.cpp
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <vector>
 
+#include "lib_sort.hpp"
 #include "mem_ops.hpp"
 #include "user_types.h"
 
@@ -24,55 +25,6 @@ void get_order(double ** mat, int n, double * order_arr) {
         order_arr[row] = order;
     }
 }
-
-void merge(oa_elem_t A[], int p, int q, int r) {
-    int size_r, size_l;
-    int i, j;
-    size_l = q - p + 1;
-    size_r = r - q;
-    oa_elem_t L[size_l + 1];
-    oa_elem_t R[size_r + 1];
-    i = 0;
-    j = 0;
-    for(int n = p; n < q + 1; ++n) {
-        L[i] = A[n];
-        ++i;
-    }
-    L[size_l].val = MAX_INT;
-    for(int n = q + 1; n < r + 1; ++n) {
-        R[j] = A[n];
-        ++j;
-    }
-    R[size_r].val = MAX_INT;
-    i = 0;
-    j = 0;
-    for(int n = p; n < r + 1; ++n) {
-        if(L[i].val < R[j].val) {
-            A[n] = L[i];
-            ++i;
-        }
-        else {
-            A[n] = R[j];
-            ++j;
-        }
-    }
-}
-
-void merge_sort(oa_elem_t A[], int p, int r) {
-    int q;
-    if(p < r) {
-        q = (p + r)/2;
-        merge_sort(A, p, q);
-        merge_sort(A, q + 1, r);
-        merge(A, p, q, r);
-    }
-}
-
-void mergesort(oa_elem_t A[], int size) {
-    merge_sort(A, 0, size - 1);
-}
-
-
 
 void make_ordered_mat(double ** mat, int n, double * order_arr, double ** ordered_mat) {
 
@@ -171,13 +123,15 @@ void gauss_jordan(double ** mat, int n, double ** mat_inv) {
     // Cut low values out of matrix
     cut_values(mat, n);
 
-    // Initial sort of input matrix
+    // Get row orders of input matrix
     get_order(mat, n, order_arr);
 
+    // Get sorted matrices
     make_ordered_mat(mat, n, order_arr, mat_ordered);
 
     make_ordered_mat(mat_inv, n, order_arr, mat_inv_ordered);
 
+    // Set matrices to ordered matrices
     for(int row = 0; row < n; ++row) {
         for(int c = 0; c < n; ++c) {
             mat_ref[row][c] = mat_ordered[row][c];
@@ -196,12 +150,15 @@ void gauss_jordan(double ** mat, int n, double ** mat_inv) {
 
         // Sort if under threshold
         if(fabs(mat_ref[c][c]) < SMALL_NUM) {
+            // Get row orders of coefficient matrix
             get_order(mat_ref, n, order_arr);
 
+            // Get sorted matrices
             make_ordered_mat(mat_ref, n, order_arr, mat_ordered);
 
             make_ordered_mat(mat_inv, n, order_arr, mat_inv_ordered);
 
+            // Set matrices to ordered matrices
             for(int row = 0; row < n; ++row) {
                 for(int c = 0; c < n; ++c) {
                     mat_ref[row][c] = mat_ordered[row][c];
---
+++ two-bulb-impn/lib_sort.hpp
@@ -0,0 +1,16 @@
+/*
+ * lib_sort.hpp
+ *
+ *  Created on: Apr 20, 2022
+ *      Author: d-w-h
+ */
+
+#ifndef TWO_BULB_IMPN_LIB_SORT_HPP_
+#define TWO_BULB_IMPN_LIB_SORT_HPP_
+
+#include "user_types.h"
+
+void mergesort(oa_elem_t A[], int size);
+
+
+#endif /* TWO_BULB_IMPN_LIB_SORT_HPP_ */
