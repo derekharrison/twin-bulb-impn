cd9ec63338db1d28a52f52c3b9bf201caec633da
Derek <harrison.derek.w@gmail.com>
2022-05-08 14:55:34 +0200

    refactor gauss-jordan procedure

--- two-bulb-impn/lib_gauss.cpp
+++ two-bulb-impn/lib_gauss.cpp
@@ -25,7 +25,7 @@ void get_order(double ** mat, int n, double * order_arr) {
     }
 }
 
-void make_ordered_mat(double ** mat, int n, double * order_arr, double ** ordered_mat) {
+void sort_mat(double ** mat, int n, double * order_arr, double ** ordered_mat) {
 
     oa_elem_t * order_array = new oa_elem_t[n];
 
@@ -60,34 +60,36 @@ int count_leading_zeros(double ** mat, int n, int row) {
     return count;
 }
 
-void singularity_check(double ** mat_ref, int n, bool & mat_is_singular) {
-    // Check for singularity
+void singularity_check(double ** mat_ref, int n, bool & is_singular) {
+
     for(int row = 0; row < n; ++row) {
-        bool all_zeros_r = true;
+        bool all_zeros_c = true;
         for(int col = 0; col < n; ++col) {
-            if(fabs(mat_ref[row][col]) > SMALL_NUM)
-                all_zeros_r = false;
+            if(fabs(mat_ref[row][col]) > SMALL_NUM) {
+                all_zeros_c = false;
+            }
         }
-        if(all_zeros_r && !mat_is_singular) {
+        if(all_zeros_c && !is_singular) {
             printf("Matrix is singular\n");
-            mat_is_singular = true;
+            is_singular = true;
         }
     }
 
     for(int col = 0; col < n; ++col) {
-        bool all_zeros_c = true;
+        bool all_zeros_r = true;
         for(int row = 0; row < n; ++row) {
-            if(fabs(mat_ref[row][col]) > SMALL_NUM)
-                all_zeros_c = false;
+            if(fabs(mat_ref[row][col]) > SMALL_NUM) {
+                all_zeros_r = false;
+            }
         }
-        if(all_zeros_c && !mat_is_singular) {
+        if(all_zeros_r && !is_singular) {
             printf("Matrix is singular\n");
-            mat_is_singular = true;
+            is_singular = true;
         }
     }
 }
 
-void cut_values(double ** mat, int n) {
+void cut_low_vals(double ** mat, int n) {
 
     for(int row = 0; row < n; ++row) {
         for(int col = 0; col < n; ++col) {
@@ -98,14 +100,22 @@ void cut_values(double ** mat, int n) {
     }
 }
 
-void gauss_jordan(double ** mat, int n, double ** mat_inv) {
+void sort_matrix(double * order_arr, int n, double ** mat) {
 
-    double ** mat_ref = mat2D(n);
     double ** mat_ordered = mat2D(n);
-    double ** mat_inv_ordered = mat2D(n);
-    double * order_arr = new double[n];
 
-    // Initialize matrix inverse
+    sort_mat(mat, n, order_arr, mat_ordered);
+
+    for(int row = 0; row < n; ++row) {
+        for(int c = 0; c < n; ++c) {
+            mat[row][c] = mat_ordered[row][c];
+        }
+    }
+
+    free_mat2D(mat_ordered, n);
+}
+
+void init_mat_inv(double ** mat_inv, int n) {
     for(int row = 0; row < n; ++row) {
         for(int c = 0; c < n; ++c) {
             if(c == row) {
@@ -116,107 +126,83 @@ void gauss_jordan(double ** mat, int n, double ** mat_inv) {
             }
         }
     }
+}
 
-    // Cut low values out of matrix
-    cut_values(mat, n);
-
-    // Get row orders of input matrix
-    get_order(mat, n, order_arr);
-
-    // Get sorted matrices
-    make_ordered_mat(mat, n, order_arr, mat_ordered);
-
-    make_ordered_mat(mat_inv, n, order_arr, mat_inv_ordered);
-
-    // Set matrices to ordered matrices
+void check_leading_zeros(double ** mat, int n, bool & is_singular) {
+    // Check if matrix is singular
     for(int row = 0; row < n; ++row) {
-        for(int c = 0; c < n; ++c) {
-            mat_ref[row][c] = mat_ordered[row][c];
-            mat_inv[row][c] = mat_inv_ordered[row][c];
+        int num_lead_zeros = count_leading_zeros(mat, n, row);
+
+        if(num_lead_zeros >= row + 1 && !is_singular) {
+            printf("Matrix is singular\n");
+            is_singular = true;
         }
     }
+}
+
+void gauss_jordan(double ** mat, int n, double ** mat_inv) {
+
+    double * order_arr = new double[n];
+
+    // Initialize matrix inverse
+    init_mat_inv(mat_inv, n);
 
     // Initialize singularity flag
-    bool mat_is_singular = false;
-
-    // Check if input matrix is singular
-    singularity_check(mat_ref, n, mat_is_singular);
+    bool is_singular = false;
 
     // Convert to row echelon form
     for(int c = 0; c < n; ++c) {
 
         // Sort if under threshold
-        if(fabs(mat_ref[c][c]) <= SMALL_NUM) {
-            // Get row orders of coefficient matrix
-            get_order(mat_ref, n, order_arr);
+        if(fabs(mat[c][c]) <= SMALL_NUM) {
+            get_order(mat, n, order_arr);
 
-            // Get sorted matrices
-            make_ordered_mat(mat_ref, n, order_arr, mat_ordered);
+            sort_matrix(order_arr, n, mat);
 
-            make_ordered_mat(mat_inv, n, order_arr, mat_inv_ordered);
+            sort_matrix(order_arr, n, mat_inv);
 
-            // Set matrices to ordered matrices
-            for(int row = 0; row < n; ++row) {
-                for(int c = 0; c < n; ++c) {
-                    mat_ref[row][c] = mat_ordered[row][c];
-                    mat_inv[row][c] = mat_inv_ordered[row][c];
-                }
-            }
+            check_leading_zeros(mat, n, is_singular);
         }
 
         // Normalize matrix row
         for(int col = c + 1; col < n; ++col) {
-            mat_ref[c][col] = mat_ref[c][col] / (mat_ref[c][c] + SMALL_NUM);
+            mat[c][col] = mat[c][col] / (mat[c][c] + SMALL_NUM);
         }
 
         // Update row matrix inverse
         for(int col = 0; col < n; ++col) {
-            mat_inv[c][col] = mat_inv[c][col] / (mat_ref[c][c] + SMALL_NUM);
+            mat_inv[c][col] = mat_inv[c][col] / (mat[c][c] + SMALL_NUM);
         }
 
-        mat_ref[c][c] = 1.0;
+        mat[c][c] = 1.0;
 
         // Delete elements in rows below
         for(int row = c + 1; row < n; ++row) {
-            if(mat_ref[row][c] != 0) {
+            if(mat[row][c] != 0) {
                 for(int col = c + 1; col < n; ++col) {
-                    mat_ref[row][col] = -1.0 * mat_ref[row][c] * mat_ref[c][col] + mat_ref[row][col];
+                    mat[row][col] = -1.0 * mat[row][c] * mat[c][col] + mat[row][col];
                 }
                 for(int col = 0; col < n; ++col) {
-                    mat_inv[row][col] = -1.0 * mat_ref[row][c] * mat_inv[c][col] + mat_inv[row][col];
+                    mat_inv[row][col] = -1.0 * mat[row][c] * mat_inv[c][col] + mat_inv[row][col];
                 }
-                mat_ref[row][c] = 0;
-            }
-
-            int num_lead_zeros = count_leading_zeros(mat_ref, n, row);
-
-            if(num_lead_zeros == n && !mat_is_singular) {
-                printf("Matrix is singular\n");
-                mat_is_singular = true;
+                mat[row][c] = 0;
             }
         }
-
     }
 
-    // Backtracing
+    // Backtrace to convert to reduced row echelon form
     for(int c = n - 1; c > 0; --c) {
-        for(int row = c - 1; row > -1; row--) {
-            if(mat_ref[row][c] != 0) {
-                for(int col = 0; col < n; col++) {
-                    mat_inv[row][col] = -1.0 * mat_ref[row][c] * mat_inv[c][col] + mat_inv[row][col];
+        for(int row = c - 1; row > -1; --row) {
+            if(mat[row][c] != 0) {
+                for(int col = 0; col < n; ++col) {
+                    mat_inv[row][col] = -1.0 * mat[row][c] * mat_inv[c][col] + mat_inv[row][col];
                 }
-                mat_ref[row][c] = 0;
+                mat[row][c] = 0;
             }
         }
     }
-
-    // Check if matrix is singular
-    singularity_check(mat_ref, n, mat_is_singular);
 
     // Free allocated space
-    free_mat2D(mat_ref, n);
-    free_mat2D(mat_ordered, n);
-    free_mat2D(mat_inv_ordered, n);
     delete [] order_arr;
 }
 
